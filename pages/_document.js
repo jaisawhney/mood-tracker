6ff65b78876ccfc385302141a836fe99e7e5e730
Jai Sawhney <jai.sawhney1@gmail.com>
2023-01-15 04:18:31 -0800

    Add splash screens

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,11 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-const APP_NAME = 'Mood Tracker'
-const APP_DESCRIPTION = 'Track your moods'
+const APP_NAME = 'Mood Tracker';
+const APP_DESCRIPTION = 'Track your moods';
 
 class _Document extends Document {
     static async getInitialProps(ctx) {
-        return await Document.getInitialProps(ctx)
+        return await Document.getInitialProps(ctx);
     }
 
     render() {
@@ -23,14 +23,30 @@ class _Document extends Document {
                     <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
                     <link rel='manifest' href='/manifest.json' />
                     <link rel='shortcut icon' href='/favicon.ico' />
+
+                    {/* Splash Screens */}
+                    <link rel='apple-touch-startup-image' href='images/splash/splash-640x1136.png'
+                          media='(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)' />
+                    <link rel='apple-touch-startup-image' href='images/splash/splash-750x1294.png'
+                          media='(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)' />
+                    <link rel='apple-touch-startup-image' href='images/splash/splash-1242x2148.png'
+                          media='(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)' />
+                    <link rel='apple-touch-startup-image' href='images/splash/splash-1125x2436.png'
+                          media='(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)' />
+                    <link rel='apple-touch-startup-image' href='images/splash/splash-1536x2048.png'
+                          media='(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)' />
+                    <link rel='apple-touch-startup-image' href='images/splash/splash-1668x2224.png'
+                          media='(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)' />
+                    <link rel='apple-touch-startup-image' href='images/splash/splash-2048x2732.png'
+                          media='(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)' />
                 </Head>
                 <body>
                     <Main />
                     <NextScript />
                 </body>
             </Html>
-        )
+        );
     }
 }
 
-export default _Document
+export default _Document;
